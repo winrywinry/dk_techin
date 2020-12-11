3084b70ad92608877ba3daf31c3941b49d2b8f14
suyeon <rai007@polarium.co.kr>
2020-12-11 13:44:30 +0900

    add gitignore

--- src/com/winry/Main.java
+++ src/com/winry/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String path = System.getProperty("user.dir");
+        System.out.println("Working Directory = " + path);
     }
 }
